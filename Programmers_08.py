3eea22f7e787c6add6370998d74d9a61b0d21c6b
UiHyeon <yh4268@koreatech.ac.kr>
2021-06-17 16:23:22 +0900

    refactor: Programmers 수식 최대화 문제 update

--- Programmers_08.py
+++ Programmers_08.py
@@ -1,40 +1,47 @@
 from itertools import permutations
+import re
 
-def cal_oper(num1,num2,oper):
-    if oper == '+':
+def cal_test(op, num1, num2):
+    if op == '+':
         return num1 + num2
-    if oper == '-':
+    if op == '-':
         return num1 - num2
-    if oper = '*':
+    if op == '*':
         return num1 * num2
 
 def solution(expression):
     answer = 0
     cal = ['+', '-', '*']
     cal_temp = list(permutations(cal,3))
+    print(cal_temp)
     max_num = 0
-    number = ''
+    # expression 연산자 기준 쪼개기
+    exp = re.findall("(\d+)([*+-]?)", expression)
+    exp_temp = []
+    for temp in exp:
+        exp_temp.append(temp[0])
+        exp_temp.append(temp[1])
     
-    front_q = []
-    end_q = [expression[i] for i in range(len(expression)-1,-1,-1)]
-    
-    print(end_q)
-    
-    while cal_temp:
-        temp = cal_temp.pop()
-        
-        for i in range(len(temp)):
-            while end_q:
-                end_pop = end_q.pop()
-                
-                if end_pop not in cal:
-                    number += end_pop
+    for oper in cal_temp:
+        exp = exp_temp[:-1]
+        exp.reverse()
+        for i in range(len(oper)):
+            result = []
+            while exp:
+                thing = exp.pop()
+                if oper[i] == thing:
+                    num1 = int(result.pop())
+                    num2 = int(exp.pop())
+                    op = oper[i]
+                    cal_num = cal_test(op,num1,num2)
+                    result.append(str(cal_num))
                 else:
-                    front_q.append(number)
-                    number = ''
+                    result.append(thing)
                     
-                    if end_pop == temp[i]:
-                        num1 = front_q.pop()
-                        num2 = end_q.pop()
+            exp = result
+            exp.reverse()
             
+        if max_num < abs(int(exp[0])):
+            max_num = abs(int(exp[0]))
+        
     return max_num
