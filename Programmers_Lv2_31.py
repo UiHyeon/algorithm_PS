1652f7f062ff0ec60f8457d2b45a9b8767532e12
UiHyeon <yh4268@koreatech.ac.kr>
2021-10-21 15:11:53 +0900

    refactor: Modify Programmers 압축 solution

--- Programmers_Lv2_31.py
+++ Programmers_Lv2_31.py
@@ -5,38 +5,24 @@ def solution(msg):
     answer = []
     alpha = list(ascii_uppercase)
     alpha_dict = defaultdict(str)
-    temp = ''
     
     for i in range(1, len(alpha) +1):
         alpha_dict[alpha[i-1]] = i
+    #위 같이 안쓰고 
+    #alpha_dict = {chr(e + 64): e for e in range(1, 27)}
+    #과 같이 ASCII코드를 사용해 dict을 구현할 수 있다.
     
-    for i in range(len(msg)):
-        if i == (len(msg) -1):
-            check = msg[i]
-            answer.append(alpha_dict[check])
-        if i == (len(temp)-1):
+    check = ''
+    for i in range(1, len(msg) + 1):
+        check += msg[i-1]
+
+        if check in alpha_dict.keys():
             continue
-        check = msg[i]
-        temp += msg[i]
-        print("==========")
-        print(check)
-        
-        for j in range(i+1, len(msg)):
-            check += msg[j]
-            temp += msg[j]
-            print(check)
-            #추가한 문자가 사전에 없으면
-            if check not in alpha_dict.keys():
-                #문자 추가하는 것을 멈춘다.
-                print(check[:-1])
-                answer.append(alpha_dict[check[:-1]])
-                temp = temp[:-1]
-                alpha_dict[check] = list(alpha_dict.values())[-1] + 1
-                break
-            
-        
-    print(answer)
-    print(alpha_dict)
-    
+        else:
+            answer.append(alpha_dict[check[:-1]])
+            alpha_dict[check] = len(alpha_dict) + 1
+            check = check[-1]
+
+    answer.append(alpha_dict[check])
     
     return answer
