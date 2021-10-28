bda0289ab414dbbe4f1100fa81c3dc0356434ce6
UiHyeon <yh4268@koreatech.ac.kr>
2021-10-28 23:26:20 +0900

    refactor: Modify Programmers 기지국 설치 solution

--- Programmers_Lv3_10.py
+++ Programmers_Lv3_10.py
@@ -2,40 +2,20 @@ import math
 
 def solution(n, stations, w):
     answer = 0
+    check = []
+    start, end = 1, 0
+    W = w * 2 + 1
 
-    apart = [False for _ in range(n)]
-    for station in stations:
-        station -= 1
-        #맨 뒤
-        if station == len(apart)-1:
-            for i in range(station-w, station+1):
-                apart[i] = True
-        #맨 앞
-        elif station == 0:
-            for i in range(station, station+w):
-                apart[i] = True
-        else:
-            for i in range(station-w, station+w+1):
-                apart[i] = True
-            
-    cnt = 0
-    result = []
-    for check in apart:
-        if not check:
-            cnt += 1
-        else:
-            if cnt != 0:
-                result.append(cnt)
-            cnt = 0
-    else:
-        if cnt != 0:
-            result.append(cnt)
+    for num in stations:
+        end = num - w - 1
+        check.append((start, end))
+        start = num + w + 1
+        
+    if start <= n:
+        check.append((start, n))
     
-    distance = w * 2 + 1
-    for item in result:
-        if math.ceil(item / distance) > 1:
-            answer += math.ceil(item / distance)
-        else:
-            answer += 1
+    for item in check:
+        distance = item[1] - item[0] + 1
+        answer += math.ceil(distance / W)
             
     return answer
